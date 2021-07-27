d5494ce552b2757387ed6941ae744c1db530186e
smheidrich <smheidrich@weltenfunktion.de>
2021-07-27 18:11:47 +0200

    Forked skim as git submod, build using setuptools
    setuptools-rust, specifically. Remaining problems: executable is put
    into the lib dir instead of somewhere within the package (but no package
    defined yet => where else?)

---
+++ setup.py
@@ -0,0 +1,35 @@
+# coding: utf-8
+from setuptools import setup
+from setuptools_rust import Binding, RustExtension
+
+setup(
+  name="skim-unicode-table",
+  # version is handled dynamically by setuptools_scm
+  use_scm_version = True,
+  description="Interactive unicode table using lotabout's skim",
+  keywords="",
+  url="",
+  author="Shahriar Heidrich",
+  author_email="smheidrich@weltenfunktion.de",
+  classifiers=[
+    "Development Status :: 3 - Alpha",
+    "Environment :: Console",
+    "Operating System :: POSIX :: Linux",
+    "Programming Language :: Python :: 3.6",
+  ],
+  rust_extensions=[RustExtension("sk", path="skim-fork/Cargo.toml",
+    binding=Binding.Exec)],
+  # Rust binary definitely not zip safe:
+  zip_safe=False,
+  # modules=[""],
+  scripts=[],
+  setup_requires=[
+    "pytest-runner",
+    "setuptools_scm",
+  ],
+  install_requires=[
+  ],
+  tests_require=[
+    "pytest",
+  ],
+)
