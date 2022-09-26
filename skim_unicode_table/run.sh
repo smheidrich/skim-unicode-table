3fac497c6631ed2ba959312b4bbd2f18bf6a8d3a
smheidrich <smheidrich@weltenfunktion.de>
2022-09-27 01:08:21 +0200

    Fix viu explicit stdin ("-") arg in ver > 0.*

--- skim_unicode_table/run.sh
+++ skim_unicode_table/run.sh
@@ -7,6 +7,12 @@ SKIM_CMD="$SCRIPT_DIR/sk"
 RENDER_GLYPH_CMD="$SCRIPT_DIR/render-glyph"
 PRINT_TABLE_CMD="$SCRIPT_DIR/print-unicode-table"
 
+if viu -V | grep 'viu 0'; then
+  viu_input_file_arg=""
+else
+  viu_input_file_arg="-"
+fi
+
 $PRINT_TABLE_CMD \
 | $SKIM_CMD -n 1,2,3,4,5,6,7  -d '  ' --with-nth 1,2 \
---preview='echo {1} | '"$RENDER_GLYPH_CMD"' | viu -w 32 ; echo {2..}'
+--preview="echo {1} | '$RENDER_GLYPH_CMD' | viu -w 32 $viu_input_file_arg; echo {2..}"
